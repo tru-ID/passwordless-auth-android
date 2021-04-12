2451dd653f29e8284b1b7cacbb4bd27eedd94866
glamboyosa <ogbemudiatimothy@gmail.com>
2021-04-12 14:02:34 +0100

    chore: cleanup in mainactivity.kt

--- app/src/main/java/com/example/tru_phonecheck/MainActivity.kt
+++ app/src/main/java/com/example/tru_phonecheck/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.tru_phonecheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import id.tru.sdk.TruSDK
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // initialize Tru SDK
-        TruSDK.initializeSdk(applicationContext)
+     
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
